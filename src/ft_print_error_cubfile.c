eb5eded97baaca22147bc941c86d7c27a1c238e7
Wincenty Bertoni Lech <Wblech@MacBook-Pro-de-Wincenty.local>
2020-10-08 17:01:01 -0300

    Arrumado norminette ft_prin_error_cubfile

--- src/ft_print_error_cubfile.c
+++ src/ft_print_error_cubfile.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_error_cubfile.c                                   :+:      :+:    :+:   */
+/*   ft_print_error_cubfile.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wbertoni <wbertoni@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/12 15:10:48 by wbertoni          #+#    #+#             */
-/*   Updated: 2020/10/05 17:21:13 by wbertoni         ###   ########.fr       */
+/*   Created: 2020/10/08 17:00:22 by wbertoni          #+#    #+#             */
+/*   Updated: 2020/10/08 17:00:40 by wbertoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubfile.h"
 
-void ft_print_error_cubfile(t_error_file error)
+static void	ft_print_error_cubfile_norminette(t_error_file error)
+{
+	if (error == enotexture)
+		printf("Error:\nCouldn´t find  texture information in cubfile\n");
+	else if (error == enocolor)
+		printf("Error:\nCouldn´t find  color information for ceiling or \
+		floor in cubfile\n");
+	else if (error == enomap)
+		printf("Error:\nCouldn´t find map information in cubfile\n");
+	else if (error == eplayer)
+		printf("Error:\nCouldn´t find initial player position on the map\n");
+}
+
+void		ft_print_error_cubfile(t_error_file error)
 {
 	if (error == eallow)
 		printf("Error\nSymbols allowed for map, without quote,\
@@ -37,12 +50,6 @@ numbers\n");
 by a ','\n");
 	else if (error == enores)
 		printf("Error:\nCouldn´t find  resolution information in cubfile\n");
-	else if (error == enotexture)
-		printf("Error:\nCouldn´t find  texture information in cubfile\n");
-	else if (error == enocolor)
-		printf("Error:\nCouldn´t find  color information for ceiling or floor in cubfile\n");
-	else if (error == enomap)
-		printf("Error:\nCouldn´t find map information in cubfile\n");
-	else if (error == eplayer)
-		printf("Error:\nCouldn´t find initial player position on the map\n");
+	else
+		ft_print_error_cubfile_norminette(error);
 }
